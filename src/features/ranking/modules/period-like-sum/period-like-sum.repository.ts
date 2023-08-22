76af89c95edadfc9cc3aea14f0c65ac91beaa9b8
nagayamajun <85071324+nagayamajun@users.noreply.github.com>
2023-08-22 09:14:32 +0900

    修正点の変更

--- src/features/ranking/modules/period-like-sum/period-like-sum.repository.ts
+++ src/features/ranking/modules/period-like-sum/period-like-sum.repository.ts
@@ -2,14 +2,19 @@ import { FAIL_TO_GET_PRODUCT } from "@/constants/constants";
 import { axiosInstance } from "@/libs/axios";
 
 export const PeriodLikeSumRepository = {
-  //上位10のproductを取得
+  // 上位10のproductを取得
   async getTopTenPeriodLikeSums() {
-    const periodLikeSums = (
-      await axiosInstance.get(`/period-like-sum`).catch(() => {
-        throw new Error(FAIL_TO_GET_PRODUCT);
-      })
-    ).data;
+    try {
+      const periodLikeSums = (
+        await axiosInstance.get(`/period-like-sum`)
+      ).data;
 
-    return periodLikeSums
+      return periodLikeSums;
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new Error(FAIL_TO_GET_PRODUCT);
+      }
+      throw error;
+    }
   }
 };
